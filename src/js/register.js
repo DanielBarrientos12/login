d351a060eb34bf1f3fdd253a47a5b9203c5c1061
DanielBarrientos12 <danielbarrientos15342@outlook.com>
2024-05-14 17:48:51 -0500

    correccion

--- src/js/register.js
+++ src/js/register.js
@@ -10,20 +10,16 @@ form.addEventListener("submit", function handleFormSubmit(event) {
 
   const email = form["email"].value;
   const password = form["password"].value;
-  const confirmPassword = form["confirm-password"].value; // Asegúrate de obtener el valor con .value
+  const confirmPassword = form["confirm-password"].value;
 
-  console.log(email, password);
   if (password === confirmPassword) {
     createUser(email, password);
   } else {
-    // Crear o actualizar un mensaje de error de contraseña no coincidente
-    let errorMessage = document.getElementById("password-error-message");
-    if (!errorMessage) {
-      errorMessage = document.createElement("p");
-      errorMessage.id = "password-error-message";
-      errorMessage.style.color = "red"; // Estilo de ejemplo, ajusta según necesites
-      form.appendChild(errorMessage); // Asumiendo que quieres añadir el mensaje al final del formulario
-    }
+    let errorMessage = document.createElement("p");
+    errorMessage.id = "password-error-message";
+    errorMessage.style.color = "#f00";
+    form.appendChild(errorMessage);
+
     errorMessage.textContent =
       "La contraseña y la confirmación de la contraseña no son iguales.";
   }
@@ -34,7 +30,6 @@ function createUser(email, password) {
   const auth = getAuth();
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Usar userCredential en lugar de user
       console.log("Cuenta creada con éxito", userCredential.user);
       alert("Cuenta creada con éxito");
     })
